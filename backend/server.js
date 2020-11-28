6d81213b991e0688904698ad4c62dac96f622d42
Robspin <robin_steeman@hotmail.com>
2020-11-28 11:50:29 +0100

    Created navbar

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 
 const __dirname = path.resolve();
 
+// redirect http => https middleware
 if (process.env.NODE_ENV === 'production') {
    app.use((req, res, next) => {
       if (req.header('x-forwarded-proto') !== 'https')
