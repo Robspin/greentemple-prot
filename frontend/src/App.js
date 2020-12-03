5450a15d7b539822964d2e23d3e81d10fecc6b33
Robspin <robin_steeman@hotmail.com>
2020-12-03 09:26:23 +0100

    Created header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 
 import Nav from './components/Nav';
 import TradingViewChart from './components/TradingViewChart';
+import Header from './components/Header';
 
 const App = () => {
-   const [page, setPage] = useState('chart');
+   const [page, setPage] = useState('blog');
 
    return (
       <div>
          <Nav setPage={setPage} page={page} />
          <div className='app-body'>
             {page === 'chart' ? <TradingViewChart /> : undefined}
+            {page === 'blog' ? <Header /> : undefined}
          </div>
       </div>
    );
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+   return (
+      <div className='header'>
+         <div className='header__text-box'>
+            <h2 className='header__title'>CREATE WEALTH</h2>
+            <h3 className='header__title-secondary'>
+               THROUGH PRINCIPLES AND DISCIPLINE
+            </h3>
+         </div>
+      </div>
+   );
+};
+
+export default Header;
