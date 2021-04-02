8395ceb7702ef63e32d4bf8c47d0e2091ff0eb01
Robspin <robin_steeman@hotmail.com>
2021-04-02 10:23:38 +0200

    added leaves animation for trying

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import leaf1 from '../images/leaves1.png';
+import leaf2 from '../images/leaves2.png';
+import leaf3 from '../images/leaves3.png';
+import leaf4 from '../images/leaves4.png';
 
 const Header = ({ page, setPage }) => {
    return (
@@ -11,6 +15,90 @@ const Header = ({ page, setPage }) => {
                THROUGH PRINCIPLES AND DISCIPLINE
             </h3>
          </div>
+         <section>
+            <div className='set'>
+               <div>
+                  <img src={leaf1} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf2} alt='leaf' />
+               </div>
+               {/* <div>
+                  <img src={leaf3} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf4} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf1} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf2} alt='leaf' /> 
+               </div>
+                  */}
+               <div>
+                  <img src={leaf3} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf4} alt='leaf' />
+               </div>
+            </div>
+            <div className='set set2'>
+               <div>
+                  <img src={leaf1} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf2} alt='leaf' />
+               </div>
+               {/* <div>
+                  <img src={leaf3} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf4} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf1} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf2} alt='leaf' /> 
+               </div>
+                  */}
+               <div>
+                  <img src={leaf3} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf4} alt='leaf' />
+               </div>
+            </div>
+            <div className='set set3'>
+               <div>
+                  <img src={leaf1} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf2} alt='leaf' />
+               </div>
+               {/* 
+               <div>
+               <img src={leaf3} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf4} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf1} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf2} alt='leaf' /> 
+               </div>
+                  */}
+               <div>
+                  <img src={leaf3} alt='leaf' />
+               </div>
+               <div>
+                  <img src={leaf4} alt='leaf' />
+               </div>
+            </div>
+         </section>
       </div>
    );
 };
