75cd9fb3912ebb3d379e97282ffccffb53eefd72
Robspin <robin_steeman@hotmail.com>
2021-04-13 15:09:32 +0200

    edited blogpost and formatted for mobile

--- frontend/src/blog/GoldAndFiat.js
+++ frontend/src/blog/GoldAndFiat.js
@@ -22,8 +22,8 @@ const GoldAndFiat = () => {
             the early 1900s for example 35 dollars could be exchanged for 1
             ounce of gold.
             <br />
-            Fiat currency (fiat is Latin for “it shall be”) on the other hand is
-            not backed by anything. <br />
+            Fiat currency on the other hand (fiat is Latin for “it shall be”),
+            is not backed by anything. <br />
             And as you will find out there are great risks to this system.{' '}
             <br />
             <br />
@@ -82,7 +82,8 @@ const GoldAndFiat = () => {
             What lessons can be learned here? <br />
             Gold backed currencies keeps politicians accountable and government
             small, while Fiat currencies by their design incentivize deficit
-            spending and remove wealth from their citizens. <br />
+            spending leading to big government and devalued savings and income
+            of the citizens. <br />
             <br />
             Now let’s head back to the present, how did we end up with our
             global fiat experiment. <br />
@@ -110,18 +111,74 @@ const GoldAndFiat = () => {
          <p className='blog__text'>
             So in August 15th 1971, Richard Nixon “temporarily” suspends the
             convertibility of the dollar into gold.
-            <br />
-            Temporary, is often said by politicians, but a big reason why they
-            planned for it to be temporary it was because that they feared
-            backlash for this drastic measure. Surprisingly there was little to
-            nothing. <br />
-            Needles to say we are still on this "backed by nothing" fiat
+            <br /> <br />
+            One of the reasons why they said it would be temporary was because
+            they expected a lot of protest against this measure. Suprisingly
+            there was little to nothing. <br />
+            Needless to say we are still on this "backed by nothing" fiat
             currency today. <br />
             As a result for the past 50 years we saw gold, which used to be
             pegged at 35 dollar, rise to 1700 dollars at the time that I am
             writing this.
          </p>
-
+         <br />
+         <br />
+         <div alt='fiat' className='blog__title--image-fed' />
+         <br />
+         <p className='blog__text'>
+            The chart above represents the amount of dollars in circulation and
+            as you can see inflation has reached insane levels where more then
+            3/4 of the dollars in circulation was printed last year.
+            <br />
+            The fed has recently decided to discontinue releasing these
+            numbers...
+            <br />
+            <br />
+            So where do we go from here?
+            <br />
+            <br />
+            Well{' '}
+            <a
+               href='https://en.wikipedia.org/wiki/Gresham%27s_law'
+               target='_blank'
+               className='blog__text--link'
+            >
+               Gresham's law
+            </a>{' '}
+            would predict that people will turn to better money.
+            <br />
+            You could argue that this is Bitcoin as its value keeps rising
+            amidst this inflation.
+            <br />
+            My prediction is that governments will try to keep the public from
+            using alternatives and eventually introduce their own digital
+            currency.
+            <br />
+            <br />
+            One thing is certain, this global fiat experiment is nearing its
+            end. <br /> And I hope we don't have to suffer the same fate as the
+            Roman's did.
+         </p>
+         <br />
+         <br />
+         <br />
+         <br />
+         <div className='blog__sources'>
+            <h4>Sources:</h4>
+            <ul>
+               <li>
+                  Roman coin pictures by Rasiel at English Wikipedia, CC BY-SA
+                  3.0, https://commons.wikimedia.org/w/index.php?curid=18727192
+               </li>
+               <li>
+                  M1 Money Stock chart https://fred.stlouisfed.org/series/M1
+               </li>
+            </ul>
+         </div>
+         <br />
+         <br />
+         <br />
+         <br />
          <Link className='blog__return' to='/blog'>
             &#11176;
             <span className='blog__return--back'>back</span>
